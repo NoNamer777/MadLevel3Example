4d3efaac6102bd4ddf08599511397a6af9df5f12
Oscar Wellner <o.wellner@telfort.nl>
2020-09-15 17:41:38 +0200

    Step 6: Remove created Reminder by swiping its card left

--- app/src/main/java/com/nonamer777/madlevel3example/fragment/RemindersFragment.kt
+++ app/src/main/java/com/nonamer777/madlevel3example/fragment/RemindersFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.setFragmentResultListener
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.nonamer777.madlevel3example.R
@@ -44,8 +45,11 @@ class RemindersFragment : Fragment() {
         rvReminders.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
         rvReminders.adapter = reminderAdapter
         rvReminders.addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+
+        createItemTouchHelper().attachToRecyclerView(rvReminders)
     }
 
+    /** Observes the output of the `Add Reminder` Fragment and handles its result. */
     private fun observeAddReminderResult() {
         setFragmentResultListener(REQ_REMINDER_KEY) { key, bundle ->
             bundle.getString(BUNDLE_REMINDER_KEY)?.let {
@@ -55,5 +59,27 @@ class RemindersFragment : Fragment() {
                 reminderAdapter.notifyDataSetChanged()
             } ?: Log.e("ReminderFragment", "Request triggered, but empty reminder text!")
         }
+    }
+
+    /** Creates a touch helper to recognize when a User swipes an item in the recycler view. */
+    private fun createItemTouchHelper(): ItemTouchHelper {
+        val callback = object: ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+
+            // Disables moving an item up and down the list.
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean = false
+
+            // Callback triggered when a User swipes an item left.
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+
+                reminders.removeAt(position)
+                reminderAdapter.notifyDataSetChanged()
+            }
+        }
+        return ItemTouchHelper(callback)
     }
 }
